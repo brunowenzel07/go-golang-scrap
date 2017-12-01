39d3d92df1d4ca5cdccf62206a3291a106884068
webdev498 <gcmobile@mail.com>
2017-12-01 12:03:15 -0500

    Optimization and merge 2 APIs results into one

--- scraper_api.go
+++ scraper_api.go
@@ -13,19 +13,6 @@ import (
 	"strings"
 )
 
-
-type SubRace struct {
-	RaceId			string
-	rTime			string
-	raceDate		string
-}
-
-type RaceList struct {
-	RaceName 		string
-	TrackId			string
-	Races			[]SubRace
-}
-
 type TrackResult struct {
 	Position		string
 	Name 			string
@@ -39,6 +26,21 @@ type Track struct {
 	MediaURL		string
 
 	Results			[]TrackResult
+}
+
+type SubRace struct {
+	RaceId			string
+	rTime			string
+	raceDate		string
+
+	TrackDetail		Track
+}
+
+type RaceList struct {
+	RaceName 		string
+	TrackId			string
+
+	Races			[]SubRace
 }
 
 func sendGetRequestWithURL(url string) []byte {
@@ -70,11 +72,11 @@ func sendGetRequestWithURL(url string) []byte {
 
 func GetRaceResult() []RaceList {
 	strYesterday := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
-	url := fmt.Sprintf("http://greyhoundbet.racingpost.com/results/blocks.sd?r_date=%s&blocks=header,meetings&_=1", strYesterday);
+	getResultUrl := fmt.Sprintf("http://greyhoundbet.racingpost.com/results/blocks.sd?r_date=%s&blocks=header,meetings&_=1", strYesterday);
 
-	getResBody := sendGetRequestWithURL(url)
+	getResBody := sendGetRequestWithURL(getResultUrl)
 	if getResBody == nil {
-		fmt.Println("API call error : " + url)
+		fmt.Println("API call error : " + getResultUrl)
 		return nil
 	}
 
@@ -102,97 +104,98 @@ func GetRaceResult() []RaceList {
 				subRaceObj.rTime = strings.Split(subRace.Path("rTime").Data().(string), " ")[1]
 				subRaceObj.raceDate = strings.Split(subRace.Path("raceDate").Data().(string), " ")[0]
 
+				//Get Race Details 
+				subRaceObj = GetRaceDetailResult(subRaceObj, raceObj.TrackId)
+
 				raceObj.Races = append(raceObj.Races, subRaceObj)
 			}
 
 			raceList = append(raceList, raceObj);
+
+			// return raceList;
 		}
 	}
 
 	return raceList;
 }
 
-func GetRaceDetailResult(raceResult []RaceList) {
+func GetRaceDetailResult(subRaceObj SubRace, trackId string) SubRace{
 
-	for _, raceObj := range raceResult {
+	paramStr := "&race_id=" + subRaceObj.RaceId
+	paramStr += "&track_id=" + trackId
+	paramStr += "&r_date=" + subRaceObj.raceDate
+	paramStr += "&r_time=" + url.QueryEscape(subRaceObj.rTime)
 
-		for _, subRaceObj := range raceObj.Races {
-			paramStr := "&race_id=" + subRaceObj.RaceId
-			paramStr += "&track_id=" + raceObj.TrackId
-			paramStr += "&r_date=" + subRaceObj.raceDate
-			paramStr += "&r_time=" + url.QueryEscape(subRaceObj.rTime)
+	url := "http://greyhoundbet.racingpost.com/results/blocks.sd?blocks=meetingHeader,results-meeting-pager,list&_=1" + paramStr
+	// fmt.Println("====================================================");
+	// fmt.Println("URL : ", url);
+	// fmt.Println("Real URL : ", "http://greyhoundbet.racingpost.com/#result-meeting-result/" + paramStr);
 
-			url := "http://greyhoundbet.racingpost.com/results/blocks.sd?blocks=meetingHeader,results-meeting-pager,list&_=1" + paramStr
-			fmt.Println("====================================================");
-			fmt.Println("URL : ", url);
-			fmt.Println("Real URL : ", "http://greyhoundbet.racingpost.com/#result-meeting-result/" + paramStr);
-
-			getResBody := sendGetRequestWithURL(url)
-			if getResBody == nil {
-				fmt.Println("API call error : " + url)
-				continue
-			}
-		
-			jsonParsed, jsonErr := gabs.ParseJSON(getResBody)
-			if jsonErr != nil {
-				fmt.Println(jsonErr)
-				continue
-			}
-
-			/* ------------- Parse the details of track ------------------------ */
-			var trackObj Track
-			races, _ := jsonParsed.Path("list.track.races").Children()
-			
-			myRace := races[0]
-			for _, r := range races {
-				if r.Path("raceId").Data().(string) == subRaceObj.RaceId {
-					myRace = r
-					break
-				}
-			}
-
-			raceNumber := strings.Split(myRace.Path("raceTitle").Data().(string), " ")[1]
-			raceDistance := myRace.Path("distance").Data().(string)
-
-			mediaURL := ""
-			if len(myRace.Path("videoid").Data().(string)) > 0 {
-				mediaURL = "http://greyhoundbet.racingpost.com/#result-video/"
-				mediaURL += "race_id=" + subRaceObj.RaceId
-				mediaURL += "&track_id=" + raceObj.TrackId
-				mediaURL += "&r_date=" + subRaceObj.raceDate
-	
-				mediaURL += "&video_id=" + myRace.Path("videoid").Data().(string)
-				mediaURL += "&clip_id=" + myRace.Path("clipId").Data().(string)
-				mediaURL += "&start_sec=" + myRace.Path("startSec").Data().(string)
-				mediaURL += "&end_sec=" + myRace.Path("endSec").Data().(string)
-			}
-
-			trackObj.Number = raceNumber
-			trackObj.Distance = raceDistance
-			trackObj.MediaURL = mediaURL
-
-			results, _ := jsonParsed.Path("list.track.results." + subRaceObj.RaceId).Children()
-			for _, trackResult := range results {
-				var trackResultObj TrackResult
-
-				position := trackResult.Path("position").Data().(string)
-				name := trackResult.Path("name").Data().(string)
-				time := trackResult.Path("winnersTimeS").Data().(string)
-				dogId := trackResult.Path("dogId").Data().(string)
-
-				trackResultObj.Position = position
-				trackResultObj.Name = name
-				trackResultObj.FinishTime = time
-				trackResultObj.DogId = dogId
-
-				trackObj.Results = append(trackObj.Results, trackResultObj)
-			}
-
-
-			fmt.Println("Track Information : %v", trackObj);
-		}
-
+	getResBody := sendGetRequestWithURL(url)
+	if getResBody == nil {
+		fmt.Println("API call error : " + url)
+		return subRaceObj
 	}
+
+	jsonParsed, jsonErr := gabs.ParseJSON(getResBody)
+	if jsonErr != nil {
+		fmt.Println(jsonErr)
+		return subRaceObj
+	}
+
+	/* ------------- Parse the details of track ------------------------ */
+	var trackObj Track
+	races, _ := jsonParsed.Path("list.track.races").Children()
+	
+	myRace := races[0]
+	for _, r := range races {
+		if r.Path("raceId").Data().(string) == subRaceObj.RaceId {
+			myRace = r
+			break
+		}
+	}
+
+	raceNumber := strings.Split(myRace.Path("raceTitle").Data().(string), " ")[1]
+	raceDistance := myRace.Path("distance").Data().(string)
+
+	mediaURL := ""
+	if len(myRace.Path("videoid").Data().(string)) > 0 {
+		mediaURL = "http://greyhoundbet.racingpost.com/#result-video/"
+		mediaURL += "race_id=" + subRaceObj.RaceId
+		mediaURL += "&track_id=" + trackId
+		mediaURL += "&r_date=" + subRaceObj.raceDate
+
+		mediaURL += "&video_id=" + myRace.Path("videoid").Data().(string)
+		mediaURL += "&clip_id=" + myRace.Path("clipId").Data().(string)
+		mediaURL += "&start_sec=" + myRace.Path("startSec").Data().(string)
+		mediaURL += "&end_sec=" + myRace.Path("endSec").Data().(string)
+	}
+
+	trackObj.Number = raceNumber
+	trackObj.Distance = raceDistance
+	trackObj.MediaURL = mediaURL
+
+	results, _ := jsonParsed.Path("list.track.results." + subRaceObj.RaceId).Children()
+	for _, trackResult := range results {
+		var trackResultObj TrackResult
+
+		position := trackResult.Path("position").Data().(string)
+		name := trackResult.Path("name").Data().(string)
+		time := trackResult.Path("winnersTimeS").Data().(string)
+		dogId := trackResult.Path("dogId").Data().(string)
+
+		trackResultObj.Position = position
+		trackResultObj.Name = name
+		trackResultObj.FinishTime = time
+		trackResultObj.DogId = dogId
+
+		trackObj.Results = append(trackObj.Results, trackResultObj)
+	}
+
+	subRaceObj.TrackDetail = trackObj
+
+	return subRaceObj;
+
 }
 
 func main() {
@@ -204,8 +207,6 @@ func main() {
 		return
 	}
 
-	// fmt.Println("raceResult : ", raceResult)
-
-	GetRaceDetailResult(raceResult)
+	fmt.Println("-------------------raceResult : ", raceResult[0])
 
 }
