9ef587c59ce628717580e10a063bf1cb84e73bb4
webdev498 <gcmobile@mail.com>
2017-12-08 10:17:42 -0500

    Add the API integration functions for token fetch and venue detail

--- scraper_api.go
+++ scraper_api.go
@@ -11,6 +11,7 @@ import (
 	"io/ioutil"
 	"github.com/Jeffail/gabs"
 	"strings"
+	"bytes"
 )
 
 type DogForm struct {
@@ -62,6 +63,15 @@ type RaceList struct {
 	TrackId			string
 
 	Races			[]SubRace
+}
+
+
+
+type Venue struct {
+	venue_id		string
+	provider_id		string
+	venue_type		string
+	venue_name		string
 }
 
 func sendGetRequestWithURL(url string) []byte {
@@ -273,15 +283,109 @@ func GetDogDetail(raceId string, trackId string, dogId string, r_date string, r_
 	return dogObj
 }
 
-func main() {
+func GetVenueDetail(venueName string) Venue{
+	var venueObj Venue
 
-	raceResult := GetRaceResult()
-
-	if raceResult == nil {
-		fmt.Println("Race List is nil")
-		return
+	//Assume provider_id is given(41)
+	//Assume venue_type is given(GREYHOUND)
+	getResultUrl := fmt.Sprintf("https://staging.dw.xtradeiom.com/api/venues/search?venue_name=%s&venue_type=GREYHOUND&provider_id=41", url.QueryEscape(venueName));
+	
+	getResBody := sendGetRequestWithURL(getResultUrl)
+	if getResBody == nil {
+		fmt.Println("API call error : " + getResultUrl)
+		return venueObj
 	}
 
-	fmt.Println("-------------------raceResult : ", raceResult[0])
+	jsonParsed, jsonErr := gabs.ParseJSON(getResBody)
+	if jsonErr != nil {
+		fmt.Println(jsonErr)
+		return venueObj
+	}
+
+	venues, _ := jsonParsed.Path("data.venues").ChildrenMap()
+	for _, venue := range venues {
+		venueObj.venue_id = fmt.Sprintf("%g", venue.Path("venue_id").Data().(float64));
+		venueObj.venue_type = venue.Path("venue_type").Data().(string)
+		venueObj.venue_name = venue.Path("name").Data().(string)
+		venueObj.provider_id = "41"  //Assume provider_id is given(41)
+
+		return venueObj;
+	}
+
+	return venueObj;
+}
+
+func GetToken() string {
+	url := "https://auth.betia.co/auth"
+
+	//Build the request
+	var jsonData = `{"email":"scraper@betia.co","password":"L9x?E63h4H=6"}`;
+	var jsonStr = []byte(jsonData)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return ""
+	}
+
+	//Set the headers
+    req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return ""
+	}
+
+	defer resp.Body.Close()
+
+	body, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+		return ""
+	}
+
+	if resp.Status == "200 OK" { //Success
+		return string(body);
+	}
+	
+	return ""
+}
+
+func PostToken() string {
+	
+}
+
+func main() {
+
+	//------------Get a token------------- //
+	token := GetToken();
+	if len(token) == 0 {
+		fmt.Printf("Token fetch is Failed.")
+		return;
+	}
+	fmt.Println("-------------------Token : ", token)
+
+	//------------Get a Venue detail------------- //
+	// venueName := "Belle Vue1"
+	// venue := GetVenueDetail(venueName)
+	// if len(venue.venue_id) == 0 {
+	// 	fmt.Printf("The venue '%s' is not existing on the server", venueName)
+	// 	return;
+	// }
+	// fmt.Println("-------------------Venue Result : ", venue)
+
+	//------------Get Race result using a scrapper ------------- //
+	// raceResult := GetRaceResult()
+
+	// if raceResult == nil {
+	// 	fmt.Println("Race List is nil")
+	// 	return
+	// }
+
+	// fmt.Println("-------------------raceResult : ", raceResult[0])
 
 }
